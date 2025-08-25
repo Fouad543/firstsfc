3ec4cebea1decf03e6f4a5f67b78a771e649c6c4
Fouad543 <77285308+Fouad543@users.noreply.github.com>
2025-08-25 15:30:05 +0200

    SFC Page / Vue Components / Vue Props

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<h1>Food</h1>
+	<p>The user can now change the favorite status of the food items with the use of a new data property initialized by the 'is-favorite' prop value.</p>
+	<div id="wrapper">
+		<food-item 
+		food-name="Apples" 
+		food-desc="Apples are a type of fruit that grow on trees."
+		v-bind:is-favorite="true"/>
+		<food-item 
+		food-name="Pizza"
+		food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."
+		v-bind:is-favorite="false"/>
+		<food-item 
+		food-name="Rice"
+		food-desc="Rice is a type of grain that people like to eat."
+		v-bind:is-favorite="false"/>
+	</div> 
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script></script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style>
+	#wrapper {
+		display: flex;
+		flex-wrap: wrap;
+	}
+	#wrapper > div {
+		border: dashed black 1px;
+		flex-basis: 120px;
+		margin: 10px;
+		padding: 10px;
+		background-color: lightgreen;
+	}
+</style>               
