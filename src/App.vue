d63c90a05c87b391a6ecddbbed09b96c2c9eacd3
Fouad543 <77285308+Fouad543@users.noreply.github.com>
2025-08-26 14:49:52 +0200

    Vue emit()

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,94 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<!-- Vue / SFC / Components / Props / v-for / $ emit() (FoodItem.vue) -->
+<!-- <template>
+	<h1>Food</h1>
+	<p>The toggle button on the component emits an event to the parent "App.vue". The favorite status is modified in "App.vue", and the updated status is sent back to the component so that the image is toggled to reflect the favorite status.</p>
+	<p>The result looks exactly like before, but now the favorite staus is modified where it should be in "App.vue" instead of in the "FoodItem.vue" component.</p>
+	<div id="wrapper">
+		<food-item
+		v-for="x in foods"
+		:key="x.name"
+		:food-name="x.name"
+		:food-desc="x.desc"
+		:is-favorite="x.favorite"
+		@toggle-favorite="receiveEmit"/>
+	</div> 
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+	export default {
+		data() { 
+			return {
+				foods: [
+					{ name: 'Apples', desc: 'Apples are a type of fruit that grow on trees.', favorite: true},
+					{ name: 'Pizza', desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.', favorite: false},
+					{ name: 'Rice', desc: 'Rice is a type of grain that people like to eat.', favorite: false},
+					{ name: 'Fish', desc: 'Fish is an animal that lives in water.', favorite: true},
+					{ name: 'Cake', desc: 'Cake is something sweet that tates good.', favorite: false}
+				]
+			};
+		},
+		methods: {
+			receiveEmit(foodId) {
+				let foundFood = this.foods.find(
+				food => food.name === foodId
+			);
+			foundFood.favorite = !foundFood.favorite;
+			}
+		}
+	}
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style>
+	#wrapper {
+		display: flex;
+		flex-wrap: wrap;
+	}
+	#wrapper > div {
+		border: dashed black 1px;
+		flex-basis: 120px;
+		margin: 10px;
+		padding: 10px;
+		background-color: lightgreen;
+	}
+</style> -->
+<!-- /////////////////////////// -->
+<!-- VUE Fallthrought Attributes -->
+<!-- TodoItem.vue -->
+<!-- /////////////////////////// -->
+<template>
+  <h3>Todo List</h3>
+  <p>In this example, the style attribute falls through from App.vue into the li element which is the components root element.</p>
+  <ul>
+    <todo-item
+      v-for="x in items"
+      :key="x"
+      :item-name="x"
+      style="background-color: lightgreen;"
+    />
+  </ul>
+  <input placeholder="Add things to do here" v-model="newItem" @keydown.enter="addItem">
+  <button @click="addItem">Add</button>
+</template>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+<script>
+	export default {
+		data() { 
+			return {
+			newItem: '',
+			items: ['Buy apples','Make pizza','Mow the lawn']
+			};
+		},
+		methods: {
+			addItem() {
+			this.items.push(this.newItem);
+			this.newItem = '';
+			}
+		}
+	}
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style>
+	ul {
+		width: 150px;
+	}
+</style>    
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import TodoItem from './components/TodoItem.vue'
+// import FoodItem from './components/FoodItem.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// app.component('food-item',FoodItem)
+app.component('todo-item', TodoItem)
+app.mount('#app')
